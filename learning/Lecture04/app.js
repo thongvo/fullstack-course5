4e02ca1836a8399becdfb735f5b2d6e6c8d39c99
thongvo <hoangthongvo_1993@yahoo.com.vn>
2016-10-12 17:42:12 +0700

    Edit file for lecture04

--- learning/Lecture04/app.js
+++ learning/Lecture04/app.js
@@ -1,3 +1,12 @@
 /**
  * Created by vhthong on 12/10/2016.
  */
+(function () {
+    'use strict';
+
+    angular.module('myFirstApp',[])
+
+        .controller('MyFirstController', function() {
+
+        });
+})();
