7718378c6ba7238040a4b646f5aa1fba6a774abe
thongvo <hoangthongvo_1993@yahoo.com.vn>
2016-10-17 14:09:30 +0700

    Edit file for Lecture 15

--- learning/Lecture15/app.js
+++ learning/Lecture15/app.js
@@ -1,29 +1,39 @@
 /**
  * Created by vhthong on 13/10/2016.
  */
-(function() {
+(function () {
     'use strict';
 
     angular.module('CounterApp', [])
         .controller('CounterController', CounterController);
 
-    CounterController.$inject = ['$scope'];
-    function CounterController($scope) {
-        $scope.onceCounter = 0;
+    CounterController.$inject = ['$scope', '$timeout'];
+    function CounterController($scope, $timeout) {
         $scope.counter = 0;
-        $scope.name = "Thong";
-
-        $scope.showNumberOfWatchers = function() {
-            console.log("# of watchers: " + $scope.$$watchersCount);
-        };
-
-        $scope.countOnce = function () {
-            $scope.onceCounter = 1;
-        };
 
         $scope.upCounter = function () {
-            $scope.counter++;
+            $timeout(function () {
+                $scope.counter++;
+                console.log("Counter incremented...");
+            }, 2000);
         };
+
+
+        //$scope.upCounter = function () {
+        //    setTimeout(function () {
+        //        $scope.counter++;
+        //        console.log("Counter incremented...");
+        //    }, 2000);
+        //};
+
+        //$scope.upCounter = function () {
+        //    setTimeout(function () {
+        //        $scope.$apply(function () {
+        //            $scope.counter++;
+        //            console.log("Counter incremented...");
+        //        });
+        //    }, 2000);
+        //};
 
         //$scope.$watch('onceCounter', function (newValue, oldValue) {
         //    console.log("Oncecounter old value: " + oldValue);
@@ -35,8 +45,5 @@
         //    console.log("Counter new value: " + newValue);
         //});
 
-        $scope.$watch(function () {
-            console.log("Digest loop fired!")
-        })
     };
-}) ();
+})();
