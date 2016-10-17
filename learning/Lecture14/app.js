b9864ea7e443559cab98f53b37c7b13f0fa80802
thongvo <hoangthongvo_1993@yahoo.com.vn>
2016-10-17 13:56:23 +0700

    Edit file for Lecture 14

--- learning/Lecture14/app.js
+++ learning/Lecture14/app.js
@@ -4,23 +4,39 @@
 (function() {
     'use strict';
 
-    angular.module('DIApp', [])
-        .controller('DIController', DIController);
+    angular.module('CounterApp', [])
+        .controller('CounterController', CounterController);
 
-    function DIController($scope, $filter, $injector) {
+    CounterController.$inject = ['$scope'];
+    function CounterController($scope) {
+        $scope.onceCounter = 0;
+        $scope.counter = 0;
         $scope.name = "Thong";
 
-        $scope.upper = function () {
-            var upCase = $filter('uppercase');
-            $scope.name = upCase($scope.name);
+        $scope.showNumberOfWatchers = function() {
+            console.log("# of watchers: " + $scope.$$watchersCount);
         };
 
-        console.log($injector.annotate(DIController));
-    };
+        $scope.countOnce = function () {
+            $scope.onceCounter = 1;
+        };
 
-    function AnnonateMe(name, job, blah) {
-        return "Blah!";
-    };
+        $scope.upCounter = function () {
+            $scope.counter++;
+        };
 
-    console.log(AnnonateMe());
+        //$scope.$watch('onceCounter', function (newValue, oldValue) {
+        //    console.log("Oncecounter old value: " + oldValue);
+        //    console.log("Oncecounter new value: " + newValue);
+        //});
+        //
+        //$scope.$watch('counter', function (newValue, oldValue) {
+        //    console.log("Counter old value: " + oldValue);
+        //    console.log("Counter new value: " + newValue);
+        //});
+
+        $scope.$watch(function () {
+            console.log("Digest loop fired!")
+        })
+    };
 }) ();
