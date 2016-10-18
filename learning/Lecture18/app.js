6c55961b5ad9ab34581419054dd85d7565124b49
thongvo <hoangthongvo_1993@yahoo.com.vn>
2016-10-18 09:06:56 +0700

    Edit file for Lecture 18

--- learning/Lecture18/app.js
+++ learning/Lecture18/app.js
@@ -4,22 +4,18 @@
 (function () {
     'use strict';
 
+    var shoppingList = [
+        "Milk", "Donuts", "Cookies", "Chocolate", "Peanut Butter", "Pepto Bismol", "Pepto Bismol (Chocolate flavor)",
+        "Pepto Bismol (Cookie flavor)"
+    ];
+
     angular.module('ShoppingListApp', [])
         .controller('ShoppingListController', ShoppingListController);
 
     ShoppingListController.$inject = ['$scope'];
 
     function ShoppingListController($scope) {
-        $scope.shoppingList1 = shoppingList1;
-        $scope.shoppingList2 = shoppingList2;
-        
-        $scope.addToList = function () {
-            var newItem = {
-                name: $scope.newItemName,
-                quantity: $scope.newItemQuantity
-            };
+        $scope.shoppingList = shoppingList;
 
-            $scope.shoppingList2.push(newItem);
-        }
     };
 })();
--- learning/Lecture18/filter.js
+++ learning/Lecture18/filter.js
@@ -1,3 +1,30 @@
 /**
  * Created by vhthong on 17/10/2016.
  */
+
+var numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log("Number array: ", numberArray);
+
+function above5Filter(value) {
+    return value > 5;
+}
+var filterdNumberArray = numberArray.filter(above5Filter);
+
+console.log("Filtered Number array: ", filterdNumberArray);
+
+var shoppingList = [
+    "Milk", "Donuts", "Cookies", "Chocolate", "Peanut Butter", "Pepto Bismol", "Pepto Bismol (Chocolate flavor)",
+    "Pepto Bismol (Cookie flavor)"
+];
+
+console.log("Shopping List: " + shoppingList);
+
+var searchValue = "Bismol";
+
+function containsFIlter(value) {
+    return value.indexOf(searchValue) !== -1;
+}
+
+var searchedShoppingList = shoppingList.filter(containsFIlter);
+
+console.log("Searched shopping list: " + searchedShoppingList);
